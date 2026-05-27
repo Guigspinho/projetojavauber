4b71ced5d5f203d7abbc1c9f38c795b93a1fe28e
Lukas Barone Sussa <lukasussa@gmail.com>
2026-05-27 19:27:17 -0300

    Update GerenciadorUsuario.java

--- GerenciadorUsuario.java
+++ GerenciadorUsuario.java
@@ -1,3 +1,9 @@
+/*
+Participantes
+Nome: Guilherme Gomes Pinho - RA: 10755529
+Nome: Lukas Barone Sussa - RA: 10753291
+Nome: Ryan Silva de Sousa - RA: 10757255
+*/
 import java.util.ArrayList;
 import java.util.Scanner;
 
